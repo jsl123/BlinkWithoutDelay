4ea7cbbb24f3bdaaf4248c94510dfcdf53c22b18
Joerg Schmitz-Linneweber <joerg@schmitz-linneweber.de>
2018-12-18 15:24:45 +0100

    Initial commit

---
+++ BlinkWithoutDelay.ino
@@ -0,0 +1,129 @@
+#include <CmdParser.h>
+
+/* Blink without Delay
+
+ Turns on and off a light emitting diode(LED) connected to a digital
+ pin, without using the delay() function.  This means that other code
+ can run at the same time without being interrupted by the LED code.
+
+ The circuit:
+ * LED attached from pin 13 to ground.
+ * Note: on most Arduinos, there is already an LED on the board
+ that's attached to pin 13, so no hardware is needed for this example.
+
+
+ created 2005
+ by David A. Mellis
+ modified 8 Feb 2010
+ by Paul Stoffregen
+
+ This example code is in the public domain.
+
+
+ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+ */
+
+// constants won't change. Used here to set pin numbers:
+// Pin 13: Arduino has an LED connected on pin 13
+// Pin 11: Teensy 2.0 has the LED on pin 11
+// Pin  6: Teensy++ 2.0 has the LED on pin 6
+// Pin 13: Teensy 3.0 has the LED on pin 13
+const int ledPin =  13;      // the number of the LED pin
+boolean running = true;
+CmdParser cmd(&pCB);
+
+// Variables will change:
+int ledState = LOW;             // ledState used to set the LED
+unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long currentMillis;
+
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 200;           // interval at which to blink (milliseconds)
+
+void(* resetFunc) (void) = 0;
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
+  while (!Serial) {
+    ;
+  }
+  cmd.init();
+}
+
+void loop()
+{
+  // check to see if it's time to blink the LED; that is, if the
+  // difference between the current time and last time you blinked
+  // the LED is bigger than the interval at which you want to
+  // blink the LED.
+  currentMillis = millis();
+
+  if (running && (currentMillis - previousMillis > interval)) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
+
+  cmd.check();
+}
+
+int pCB(char *inBuffer) {
+  boolean setLed = false;
+  int rc = 0;
+
+  if (0 == strcmp(inBuffer, "stop")) {
+    running = false;
+    Serial.println("Stopped");
+  }
+  else if (0 == strcmp(inBuffer, "start")) {
+    running = true;
+    previousMillis = currentMillis;
+    Serial.println("Started");
+  }
+  else if (0 == strcmp(inBuffer, "on")) {
+    if (! running) {
+      ledState = HIGH;
+      setLed = true;
+    }
+  }
+  else if (0 == strcmp(inBuffer, "off")) {
+    if (! running) {
+      ledState = LOW;
+      setLed = true;
+    }
+  }
+  else if (0 == strncmp(inBuffer, "rate ", 5)) {
+    int newInterval = atoi(inBuffer + 5);
+    if (newInterval > 10 && newInterval <= 1000) {
+      interval = newInterval;
+      Serial.println("New rate set");
+    }
+    else {
+      Serial.println("Rate out of bounds (10 < rate <= 1000)");
+    }
+  }
+  else if (0 == strcmp(inBuffer, "reset") || 0 == strcmp (inBuffer, "reboot")) {
+    Serial.flush();
+    delay(2000);
+    resetFunc();
+  }
+  else {
+    rc = 1;
+  } 
+
+  if (setLed) {
+    digitalWrite(ledPin, ledState);
+  }
+
+  return rc;
+}
